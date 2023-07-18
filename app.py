93e203c7a433515fbd0161abed38de3ba2f10175
Thomas Wilson <tommyhwilson3@gmail.com>
2023-07-18 15:45:45 -0500

    Started routes for backend: -Reconfigured environmental variables to suit API -Set global variables to make requests simpler -Initial search route functioning on basic level

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 from flask import (
@@ -6,7 +7,7 @@ from flask import (
 )
 
 from models import (
-    db, connect_db
+    db, connect_db, User, Movie, Bucket
 )
 
 load_dotenv()
@@ -15,8 +16,31 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['API_KEY'] = os.environ['API_KEY']
+app.config['AUTH_KEY'] = os.environ['AUTH_KEY']
 
 connect_db(app)
 
+AUTH_KEY = app.config['AUTH_KEY']
+
+HEADERS = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {AUTH_KEY}"
+}
+
+BASE_API_URL = "https://api.themoviedb.org/3/"
+
 ########################################################
+
+@app.route('/api/search')
+def list_search_results():
+    """Returns JSON list of search results"""
+
+    query = request.args.get('query')
+
+    url = f"{BASE_API_URL}search/movie"
+    params = {"query": query}
+
+    response = requests.get(url, params=params, headers=HEADERS)
+
+    return jsonify(response.json())
