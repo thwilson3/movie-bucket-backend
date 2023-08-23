629cf1de20b7170cbd325624f86b8994f962fce1
Thomas Wilson <tommyhwilson3@gmail.com>
2023-08-23 14:48:48 -0500

    Newly created file to store helper functions

---
+++ helpers.py
@@ -0,0 +1,37 @@
+from models import db, Bucket, User_Buckets
+from collections import namedtuple
+
+"""Defines shape of data for given bucket"""
+BucketData = namedtuple("BucketData", ["bucket_name", "genre", "description"])
+
+
+def create_bucket(bucket_name, genre, description):
+    """Create new Bucket instance and add to database"""
+
+    try:
+        new_bucket = Bucket(
+            bucket_name=bucket_name,
+            genre=genre,
+            description=description
+        )
+
+        db.session.add(new_bucket)
+        db.session.commit()
+
+        return new_bucket
+
+    except:
+        raise Exception
+
+
+def associate_user_with_bucket(user_id, bucket_id):
+    """Create association between user and newly made bucket"""
+
+    try:
+        user_bucket = User_Buckets(user_id=user_id, bucket_id=bucket_id)
+        db.session.add(user_bucket)
+        db.session.commit()
+    except:
+        raise Exception
+
+    return True
